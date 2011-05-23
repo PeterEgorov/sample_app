bcdc28976b6f5de4c3e9e3ac050a4af0efb02b32
Peter Egorov <iitlab@gmail.com>
2011-05-23 15:36:37 +0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Главная"
   end
 
   def contact
+    @title = "Контакты"
   end
 
   def about
+    @title = "About"
   end
 
 end
