5cde785a9071746cff97349634fac4882cf06be1
PeterEgorov <iitlab@gmail.com>
2011-06-30 11:53:43 +0400

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,14 +20,36 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
 
   private
 
-    def user_from_remember_token
+  def user_from_remember_token
       User.authenticate_with_salt(*remember_token)
-    end
+  end
 
-    def remember_token
+  def remember_token
       cookies.signed[:remember_token] || [nil, nil]
-    end
+  end
+  
+  def store_location
+      session[:return_to] = request.fullpath
+  end
+
+  def clear_return_to
+      session[:return_to] = nil
+  end
 end
